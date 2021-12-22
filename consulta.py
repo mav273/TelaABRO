ccfea03a69a366e702bb7bc9eb35a8ba51ca5574
marcus <marcusvslourenco@fac.pe.senac.br>
2021-12-22 10:40:59 -0300

    organizando o código

--- consulta.py
+++ consulta.py
@@ -3,6 +3,7 @@ from template.template_anamnese import anamnesetemp0,anamnesetemp1
 from template.template_sociais import sociaistemp0,sociaistemp1
 import streamlit_authenticator as stauth
 from template.template_cadastro import cadastro_temp
+from func import delete_anamnese,delete_data,delete_sociais,get_coluna,get_coluna_a,get_coluna_s,update,update_a,update_s,cadastro_pnome,sociais_pnome,anamnese_pnome,ver_nomes
 
 #conexão com o banco
 
@@ -28,82 +29,6 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 
-# Funções_______________________________________________________________________________________________________
-
-
-
-#Deletar
-
-def delete_data(nome):
-	cursor.execute('DELETE FROM cadastro WHERE nome="{}"'.format(nome))
-	cnxn.commit()
-
-def delete_anamnese(nome):
-	cursor.execute('DELETE FROM anamnese1 WHERE nome=?'.format(nome))
-	cnxn.commit()
-
-def delete_sociais(nome):
-	cursor.execute('DELETE FROM sociais WHERE nome="{}"'.format(nome))
-	cnxn.commit()
-
-
-
-#Nomes das Colunas
-def get_coluna():
-	cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='sql10459891' AND TABLE_NAME='cadastro' ")
-	data = cursor.fetchall()
-	return data
-
-
-def get_coluna_a():
-	cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='sql10459891' AND TABLE_NAME='anamnese1' ")
-	data = cursor.fetchall()
-	return data
-
-def get_coluna_s():
-	cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='sql10459891' AND TABLE_NAME='sociais' ")
-	data = cursor.fetchall()
-	return data
-
-#Alterar
-
-def update(coluna,novo,nome):
-    cursor.execute("update cadastro set `{}` = '{}' where nome = '{}'".format(coluna,novo,nome))
-    cnxn.commit()
-
-def update_a(coluna,novo,nome):
-    cursor.execute("update anamnese1 set `{}` = '{}' where nome = '{}'".format(coluna,novo,nome))
-    cnxn.commit()
-
-def update_s(coluna,novo,nome):
-    cursor.execute("update sociais set `{}` = '{}' where nome = '{}'".format(coluna,novo,nome))
-    cnxn.commit()
-
-#Consultar os dados
-def cadastro_pnome(nome):
-	cursor.execute('SELECT * FROM cadastro WHERE nome="{}"'.format(nome))
-	data = cursor.fetchall()
-	return data
-
-def anamnese_pnome(nome):
-    cursor.execute('SELECT * FROM anamnese1 WHERE nome="{}"'.format(nome))
-    data = cursor.fetchall()
-    return data
-
-def sociais_pnome(nome):
-    cursor.execute('SELECT * FROM sociais WHERE nome="{}"'.format(nome))
-    data = cursor.fetchall()
-    return data
-
-def ver_nomes():
-	cursor.execute('SELECT nome FROM cadastro')
-	data = cursor.fetchall()
-	return data
-
-def consulta():
-    cursor.execute('Select * from cadastro')
-    data = cursor.fetchall()
-    return data
 
 
 #Login
